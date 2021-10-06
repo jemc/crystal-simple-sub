8abaa2011c5cddd35e3d5856f4bb95aae78bbe03
Joe Eli McIlvain <joe.eli.mac@gmail.com>
2021-10-06 09:07:23 -0700

    Use an expectation object to reduce testing boilerplate.

--- spec/simple_sub/typer_spec.cr
+++ spec/simple_sub/typer_spec.cr
@@ -1,8 +1,10 @@
 require "../spec_helper"
 
 describe Typer do
+  # These test cases are identical to those found here:
+  # - https://github.com/LPTK/simple-sub/blob/4ec1af41dc3796622fa16ab3ca331d10ee2a12d4/shared/src/test/scala/simplesub/TypingTests.scala#L14-L22
   it "determines the obvious type of some basic terms" do
-    typer = Typer.new
-    typer.type_term(TermLit.new(42)).show.should eq("int")
+    TermLit.new(42)
+      .should have_type("int")
   end
 end
--- spec/spec_helper.cr
+++ spec/spec_helper.cr
@@ -2,3 +2,37 @@ require "spec"
 require "../src/simple_sub"
 
 include SimpleSub
+
+# Introduce an expectation that the left side term should be inferred by
+# the SimpleSub::Typer as having a type whose string matches the right side.
+def have_type(type_string)
+  SimpleSub::TypeExpectation.new(type_string)
+end
+
+class SimpleSub::TypeExpectation
+  @type_string : String
+
+  def initialize(@type_string)
+  end
+
+  def actual_type_string(term : Term)
+    Typer.new.type_term(term).show
+  end
+
+  def match(term)
+    @type_string == actual_type_string(term)
+  end
+
+  def failure_message(term : Term)
+    <<-MSG
+      Expected type: #{@type_string}
+           got type: #{actual_type_string(term)}
+    MSG
+  end
+
+  def negative_failure_message(term : Term)
+    <<-MSG
+      Expected not to have type: #{@type_string}
+    MSG
+  end
+end
